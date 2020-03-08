987b40380382f531d5cb93bd64f4d91fa3327943
Sohel Shaikh <sohelshaikh97@gmail.com>
2020-03-08 13:38:04 +0530

    Adds more tests

---
+++ app/src/androidTest/java/com/example/github_actions_test/ArithmeticTest.java
@@ -0,0 +1,16 @@
+package com.example.github_actions_test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArithmeticTest {
+    @Test
+    public void testAddition(){
+        Assert.assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testMultiplication(){
+        Assert.assertEquals(4, 2 * 2);
+    }
+}
